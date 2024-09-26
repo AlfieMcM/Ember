984806bb09a05d331181872bb5bb7b210f25421c
MacPriory <111864550+Alfie-Mcm@users.noreply.github.com>
2024-09-26 09:26:21 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import Parser from "./frontend/parser.ts";
 import { createGlobalEnv } from "./runtime/environment.ts";
 import { evaluate } from "./runtime/interpreter.ts";
 
-run("./test.txt");
+run("./main.txt");
 
 async function run(filename: string) {
 	const parser = new Parser();
